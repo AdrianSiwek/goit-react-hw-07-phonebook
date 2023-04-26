a4afee2902a226036bf6b855012578d4c57820b8
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-04-27 00:19:14 +0200

    update and creat operations

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,7 @@
 export const getContacts = state => state.contacts;
 
 export const getFilterValue = state => state.filters.status;
+
+export const getIsLoading = state => state.tasks.isLoading;
+
+export const getError = state => state.tasks.error;
